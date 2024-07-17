2d2dbd330adf55fa74339d9a43b07073df38acef
“butsayakorn19 <butsayakorn8676@gmail.com>
2024-07-17 17:51:57 +0700

    first commit

--- src/app/chatgpt/page.tsx
+++ src/app/chatgpt/page.tsx
@@ -1,10 +1,10 @@
-import Chatgpt from '@/components/Chatgpt'
-import React from 'react'
+import Chatgpt from "@/components/Chatgpt";
+import React from "react";
 
 export default function page() {
   return (
-    <div>page
-        <Chatgpt/>
+    <div>
+      <Chatgpt />
     </div>
-  )
+  );
 }
--- src/components/Chatgpt.tsx
+++ src/components/Chatgpt.tsx
@@ -8,21 +8,18 @@ export default function Chatgpt() {
           <img
             className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
             alt="hero"
-            src="images/P1.jpg"
+            src="images/J1.jpg"
           />
           <div className="text-center lg:w-2/3 w-full">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-             AI ChatBot
+              AI chatBoT
             </h1>
-            <p className="mb-8 leading-relaxed">
-              AI ChatBoT ด้านรัฐธรรมนูญไทย
+            <p className="mb-8 leading-relaxed">AI ChatBot ด้านรัฐธรรมนูญ</p>
             <div className="flex justify-center">
               <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                 ดูรายละเอียดที่นี่
               </button>
-              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-                Button
-              </button>
+              
             </div>
           </div>
         </div>
